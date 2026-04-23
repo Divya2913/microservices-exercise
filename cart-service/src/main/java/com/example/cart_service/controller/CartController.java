c76d667200aa6ccd9643624fbab4644927478ce8
Divya Sundru <divyasundru@Divyas-MacBook-Air.local>
2026-04-23 14:08:16 -0400

    1E-controller-layer

---
+++ cart-service/src/main/java/com/example/cart_service/controller/CartController.java
@@ -0,0 +1,43 @@
+package com.example.cart_service.controller;
+
+import com.example.cart_service.entity.Cart;
+import com.example.cart_service.entity.CartItem;
+import com.example.cart_service.service.CartService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    // CREATE CART
+    @PostMapping
+    public Cart createCart(@RequestBody Cart cart) {
+        return cartService.createCart(cart);
+    }
+
+    // ADD ITEM
+    @PostMapping("/item")
+    public CartItem addItem(@RequestBody CartItem item) {
+        return cartService.addItemToCart(item);
+    }
+
+    // GET CARTS
+    @GetMapping
+    public List<Cart> getCarts() {
+        return cartService.getAllCarts();
+    }
+
+    // GET ITEMS
+    @GetMapping("/items")
+    public List<CartItem> getItems() {
+        return cartService.getAllItems();
+    }
+}
---
+++ product-service/src/main/java/com/example/product_service/controller/ProductController.java
@@ -0,0 +1,54 @@
+package com.example.product_service.controller;
+
+import com.example.product_service.entity.Product;
+import com.example.product_service.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    // CREATE
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "WORKING";
+    }
+
+    // GET ALL
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    // GET BY ID
+    @GetMapping("/{id}")
+    public Product getProduct(@PathVariable Integer id) {
+        return productService.getProductById(id);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Integer id,
+                                 @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+        productService.deleteProduct(id);
+    }
+}
